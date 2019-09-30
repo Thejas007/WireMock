3592052af9c4310b632b89b28318c48b14925872
Thejas Gowda <tnanjundaiah@vmware.com>
2019-09-30 22:55:00 +0530

    Added sample request

---
+++ WireMock/MockServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WireMock
+{
+    using RequestBuilders;
+    using ResponseBuilders;
+    using Server;
+    using Settings;
+
+    public class MockServer
+    {
+        private static FluentMockServer _mockServer;
+
+        private static string _url;
+
+        private MockServer()
+        {
+            // This method is intentionally left blank
+        }
+
+        public static FluentMockServer Server
+        {
+            get
+            {
+                if (_mockServer == null)
+                {
+                    Initialize();
+                }
+                return _mockServer;
+            }
+        }
+
+        public static void Initialize()
+        {
+            if (_mockServer == null)
+            {
+                _mockServer = FluentMockServer.Start(new FluentMockServerSettings()
+                {
+                    Urls = new[] { new UriBuilder("http", "localhost", 6000).ToString() },
+                    StartAdminInterface = true,
+                    ReadStaticMappings = true,
+                });
+
+                _url = new UriBuilder("http", "localhost", 6000).ToString();
+            }
+        }
+
+        public static string GetMockServerUrl()
+        {
+            return _url;
+        }
+
+        public static void ArrangeResponseForRequest(IRequestBuilder expectedRequest, IResponseBuilder expectedResponse)
+        {
+
+            // Add the request match pattern with the correlation Id
+            //expectedRequest.WithHeader(CORRELATION_LOOKUP_KEY, correlationId);
+
+            Server.Given(expectedRequest)
+                .RespondWith(expectedResponse);
+        }
+    }
+}
--- WireMock/Program.cs
+++ WireMock/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace WireMock
 {
+    using System.Net;
+    using RequestBuilders;
+    using ResponseBuilders;
+    using Util;
+
     class Program
     {
         static void Main(string[] args)
@@ -13,9 +18,32 @@ namespace WireMock
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+
+            MockServer.Initialize();
+            MockServer.ArrangeResponseForRequest(Request.Create().WithPath("/ping").UsingGet(),
+                Response.Create().WithCallback(GetResponse));
+
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        private static ResponseMessage GetResponse(RequestMessage request)
+        {
+            var response = new ResponseMessage
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                BodyData = new BodyData
+                {
+                    DetectedBodyType = BodyType.Json,
+                    BodyAsJsonIndented = false,
+                    BodyAsJson = new { message = "hello" }
+                }
+            };
+
+            response.AddHeader("Content-Type", "application/json");
+            return response;
         }
     }
 }
